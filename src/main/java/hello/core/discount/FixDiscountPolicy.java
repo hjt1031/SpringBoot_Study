3715f61831214d9a56546629175eea5b5227db2d
juntae ha <gkwnsxo1234@gmail.com>
2022-01-11 22:05:46 +0900

    Changes

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -5,6 +5,7 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 
 public class FixDiscountPolicy implements DiscountPolicy{
+
     private int discountFizAmount = 1000; // 1000원 할인
     @Override
     public int discount(Member member, int price) {
