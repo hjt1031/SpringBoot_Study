bda985c018005ccd828768f8698bb1e3ab10c0b2
hjt1031 <hjt1031@naver.com>
2022-02-15 17:23:37 +0900

    Spring bean 출력, javacode-> spring code로 변경

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -26,11 +26,11 @@ public class ApplicationContextInfoTest {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
-
-            // ROLE_APPLICATION => 내가 등록한 bean
+            // ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+            // ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
             if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
                 Object bean = ac.getBean(beanDefinitionName);
-                System.out.println("name = " + beanDefinitionName + " object" + bean);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
             }
         }
     }
